44f6007d87efe7d7b2f66ec74905401b83686e03
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-09-17 09:11:42 +0600

    Update: dashboard header

--- bike_servicing_center_client/src/app/(withDashboardLayout)/layout.tsx
+++ bike_servicing_center_client/src/app/(withDashboardLayout)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header";
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -7,12 +8,14 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator"
 import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Home, LogOut } from "lucide-react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
@@ -20,25 +23,15 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
             <AppSidebar />
             <SidebarInset>
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
+                    <div className="flex items-center justify-between w-full gap-2 px-4">
+                        <div className="flex items-center gap-2">
+                            <SidebarTrigger className="-ml-1" />
+                            <h2 className="text-xl font-semibold hidden md:block">AutoRapid</h2>
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <Button variant="outline" className="rounded cursor-pointer"><Home />Home</Button>
+                            <Button className="rounded cursor-pointer"><LogOut />LogOut</Button>
+                        </div>
                     </div>
                 </header>
                 {
