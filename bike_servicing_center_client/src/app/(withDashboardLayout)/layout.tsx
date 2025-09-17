23cf7d751b04892dd2613fa73d345e7fa3af6de1
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-09-17 08:58:12 +0600

    Add: Admin Dashboard Design

--- bike_servicing_center_client/src/app/(withDashboardLayout)/layout.tsx
+++ bike_servicing_center_client/src/app/(withDashboardLayout)/layout.tsx
@@ -41,11 +41,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
                         </Breadcrumb>
                     </div>
                 </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                       {children}
-                    </div>
-                </div>
+                {
+                    children
+                }
             </SidebarInset>
         </SidebarProvider>
     )
